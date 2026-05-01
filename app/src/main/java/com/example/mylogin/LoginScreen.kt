6754b629ccfbf9c94fca2e202b157c3b348fba7f
eligeraldine <5025231021@student.its.ac.id>
2026-05-02 06:29:35 +0700

    innitial commit

---
+++ app/src/main/java/com/example/mylogin/LoginScreen.kt
@@ -0,0 +1,116 @@
+package com.example.mylogin
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.*
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import android.util.Log
+
+@Composable
+fun LoginScreen(){
+
+    var email by remember {
+        mutableStateOf(value = "")
+    }
+    var password by remember {
+        mutableStateOf(value = "")
+    }
+
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = R.drawable.loginvector), contentDescription = "login image",
+            modifier = Modifier.size(200.dp))
+
+        Text(text = "Welcome Back", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(value = email, onValueChange = {
+            email = it
+        }, label = {
+            Text(text = "Email Address")
+        })
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(value = password, onValueChange = {
+            password = it
+        }, label = {
+            Text(text = "Password")
+        }, visualTransformation = PasswordVisualTransformation())
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            Log.d("StatusLogin", "Email: $email, Password: $password")
+        }) {
+            Text(text = "Login")
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(text = "Forgot Password?", modifier = Modifier.clickable {
+
+        })
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(text = "Or Sign in with")
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(40.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Image(painter = painterResource(id = R.drawable.google),
+                contentDescription = "google",
+                modifier = Modifier
+                    .size(45.dp)
+                    .clickable {
+
+                    }
+            )
+
+            Image(painter = painterResource(id = R.drawable.twitter),
+                contentDescription = "twitter",
+                modifier = Modifier
+                    .size(45.dp)
+                    .clickable {
+
+                    }
+            )
+
+            Image(painter = painterResource(id = R.drawable.facebook),
+                contentDescription = "facebook",
+                modifier = Modifier
+                    .size(45.dp)
+                    .clickable {
+
+                    }
+            )
+        }
+    }
+}
